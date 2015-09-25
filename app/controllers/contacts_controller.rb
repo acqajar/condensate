7eec2bf89e70b38a35ffc280c4904ae4a5a953ae
Arsames Qajar <acqajar@gmail.com>
2015-09-25 04:22:56 -0700

    added routing and env variable

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,14 +6,21 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+
+
+
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to root_path
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
     end
   end
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901204543) do
+ActiveRecord::Schema.define(version: 20150910213235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20150901204543) do
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "body"
+    t.string   "name"
+    t.string   "email"
+    t.string   "zipcode"
   end
 
   create_table "homes", force: :cascade do |t|
