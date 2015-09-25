1100231c270b2c043f3101ae9a6e8bdb4bd8b25f
Arsames Qajar <acqajar@gmail.com>
2015-09-25 09:54:39 -0700

    working on mailer config settings prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
    :port                 => 587,
    :user_name            => ENV['GMAIL_USERNAME'],
    :password             => ENV['GMAIL_PASSWORD'],
-   :authentication       => "plain",
+   :authentication       => :plain,
    :enable_starttls_auto => true
   }
 
