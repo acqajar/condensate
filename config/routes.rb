3fd24a358eb1cc921671fa8ba92096353a7bbb94
Arsames Qajar <acqajar@gmail.com>
2015-09-25 09:28:14 -0700

    took out about page because no copy or images

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :contacts
   resources :careers
-  resources :abouts
+  # resources :abouts
   resources :homes
 
   match '/contacts', to: 'contacts#new', via: 'get'
